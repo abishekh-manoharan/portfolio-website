54a201f5490ba3f5a29a0cea8019ebc1d6ffee6a
abi-manoharan97 <abishekh.manoharan@gmail.com>
2023-05-18 10:44:31 -0400

    included layout component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import Projects from './Projects';
 
 function Layout(props) {
     return (
-        <div>
-            <div id="navbar">
+        <div class="layout">
+            <div class="navbar">
                 <div class="navbarItem1">b</div>
                 <div class="navbarItem2">b</div>
                 <div class="navbarItem3">a</div>
+            </div>
+
+            <div class="content">
+                <Projects/>
+            </div>
+
+            <div class="footer">
+
             </div>
         </div>
     );
