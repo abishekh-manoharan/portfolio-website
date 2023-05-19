0502e0ba96797a4908467e2951986a240e2de77b
abi-manoharan97 <abishekh.manoharan@gmail.com>
2023-05-19 18:52:17 -0400

    added subheading and styles for projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -8,11 +8,27 @@ function Project(props) {
                 <div class="project-container">
                     <div class="image"><img class="project-image" src="https://placekitten.com/250/250" alt="" /></div>
                     <div class="header">{props.name}</div>
-                    <div class="description">{props.description}</div>
-                    <div class="url">{props.siteLink}</div>
-                    <div class="github">{props.githubLink}</div>
+                    <div class="description">
+                        <div class="project-subheading" style={{marginBottom: 2+'px'}}>
+                            Description
+                        </div>
+                        {props.description}
+                    </div>
+
+                    <div class="url">
+                        <div class="project-subheading">
+                            Site Link
+                        </div>
+                        {props.siteLink}
+                    </div>
+                    <div class="github">
+                        <div class="project-subheading">
+                            Github Link
+                        </div>
+                        {props.githubLink}
+                    </div>
                     <div class="tags">
-                        {props.tags.map((tag)=><TagDisplay key={props.tags.indexOf(tag)} tagName={tag}/>)}
+                        {props.tags.map((tag) => <TagDisplay key={props.tags.indexOf(tag)} tagName={tag} />)}
                     </div>
                 </div>
             </div>
