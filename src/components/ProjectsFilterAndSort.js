4f60d5ba463bfde6437fe8c25872ccfd1193ee4f
abi-manoharan97 <abishekh.manoharan@gmail.com>
2023-05-23 10:05:54 -0400

    added filter logo

--- src/components/ProjectsFilterAndSort.js
+++ src/components/ProjectsFilterAndSort.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ProjectsFilter from './ProjectsFilter';
+import dropdownLogo from '../images/dropdownLogo.png'
 
 function ProjectsFilterAndSort({ tags, handleFilter, handleCancelFilter }) {
     return (
         <div class="filterAndSort">
-            <div class="filter-header">Filter by tags:</div>
-            <ProjectsFilter tags={tags} handleFilter={handleFilter} handleCancelFilter={handleCancelFilter}/>
+            <img src={dropdownLogo} class="dropdownLogo"/>
+            <div class="filter-header">
+                Filter
+                <ProjectsFilter tags={tags} handleFilter={handleFilter} handleCancelFilter={handleCancelFilter}/>
+            </div>
             {/* TODO when doing sort functionality */}
             {/* <div class="sort">
                 SORT BYs
