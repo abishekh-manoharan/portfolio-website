123b14cf63afc1f48d428c4bd8d261bb0b63648e
abi-manoharan97 <abishekh.manoharan@gmail.com>
2023-06-02 10:47:00 -0400

    localize data

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Project from './Project';
 
 function Projects({projectsToShow}) {
+    console.log(projectsToShow);
     return (
         <div class="projects-container">
             {projectsToShow.map((e) => <Project key={e.id} name={e.name} deployed={e.deployed} description={e.description} demo={e.demo} githubLink={e.githubLink} siteLink={e.siteLink}  demoLink={e.demoLink} images={e.images} tags={e.tags} />)}
