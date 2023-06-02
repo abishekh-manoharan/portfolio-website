5172261c93682ddcb58181d1c031421c87859080
abi-manoharan97 <abishekh.manoharan@gmail.com>
2023-06-02 11:22:14 -0400

    seperate tech used from general description of projects

--- src/components/ProjectsPage.js
+++ src/components/ProjectsPage.js
@@ -7,26 +7,26 @@ import ProjectsFilterAndSort from './ProjectsFilterAndSort';
 import Project from './Project';
 
 function ProjectsPage(props) {
-    const [tags, setTags] = useState(["Website", "Mobile Application", "AI", "React", "EJS", "Express", "Firebase", "MongoDB", "NoSQL", "SQL", "CSS", "HTML", "Java", "JavaScript", "Python", "Flask", "Individual Project", "Group Project", "Academic Project", "Personal Project", "Demo", "Deployed"])
+    const [tags, setTags] = useState(["Website", "Mobile Application", "MVVM", "MVC", "AI", "React", "EJS", "Express", "Room", "MongoDB", "NoSQL", "SQL", "CSS", "HTML", "Java", "JavaScript", "Python", "Flask", "Individual Project", "Group Project", "Academic Project", "Personal Project", "Demo", "Deployed"])
     const [activeTags, setActiveTags] = useState([])
     const [projectsToShow, setProjectsToShow] = useState([])
     const [allProjects, setAllProjects] = useState([
         {
             "id": 1,
             "name": "Portfolio Website",
-            "description": "This was a personal project I developed as a means for displaying my projects to my peers and potential employers. It allows the user to view basic information about me, view a list of my projects, filter projects by tags, and to view my resume. It uses React for the front-end, Express for the back-end, and MongoDB as the database.",
+            "description": "This was a personal project I developed as a means for displaying my projects to my peers and potential employers. It allows the user to view basic information about me, view a list of my projects, filter projects by tags, and to view my resume.\n\nIt was built purely using React.",
             "deployed": false,
             "demo": false,
             "siteLink": "google.ca",
             "githubLink": "https://github.com/abishekh-manoharan/portfolio-website",
             "demoLink": "https://youtube.com",
             "images": ["https://placekitten.com/150/150","https://placekitten.com/150/150","https://placekitten.com/150/150"],
-            "tags": ["React", "Express", "HTML", "JavaScript", "MongoDB", "NoSQL", "CSS", "Individual Project", "Personal Project", "Website"]
+            "tags": ["React", "HTML", "JavaScript", "CSS", "Individual Project", "Personal Project", "Website"]
         },
         {
             "id": 2,
             "name": "CheckMyPlant Website",
-            "description": "In this academic project, I worked within a team of 8 to develop a website that allowed end users to determine if a cauliflower is diseased using an AI model which we trained ourselves. Users can upload an image of a cauliflower and receive their results on a separate page. Jupyter Notebook and Python was used to create, train, and export the AI model. Flask was used for web-page routing and as an API to make predictions. For the front-end, vanilla JavaScript, HTML, and CSS was used.",
+            "description": "In this academic project, I worked within a team of 8 to develop a website that allowed end users to determine if a cauliflower is diseased using an AI model which we trained ourselves. Users can upload an image of a cauliflower and receive their results on a separate page. \n\nJupyter Notebook and Python was used to create, train, and export the AI model. Flask was used for web-page routing and as an API to make predictions. For the front-end, vanilla JavaScript, HTML, and CSS was used.",
             "deployed": false,
             "demo": true,
             "siteLink": "google.ca",
@@ -38,7 +38,7 @@ function ProjectsPage(props) {
         {
             "id": 3,
             "name": "Student Grade Prediction Website",
-            "description": "In this academic project, me and two others created a website that allows users to predict the writing score of students based on parameters such as parental eduction level, lunch availability, and completion of a test prepatation course. We used python with Jupyter notebook to create, train, and export the model as well as to clean the data. Flask was used to create an API that returns a prediction based on specified parameters. React was used for the front-end. A more detailed explanation of the system architecture is available in the video demo linked below.",
+            "description": "In this academic project, me and two others created a website that allows users to predict the writing score of students based on parameters such as parental eduction level, lunch availability, and completion of a test prepatation course. \n\nWe used python with Jupyter notebook to create, train, and export the model as well as to clean the data. Flask was used to create an API that returns a prediction based on specified parameters. React was used for the front-end. A more detailed explanation of the system architecture is available in the video demo linked below.",
             "deployed": true,
             "demo": true,
             "siteLink": "https://aifinalprojectg4.onrender.com/",
@@ -49,39 +49,39 @@ function ProjectsPage(props) {
         },
         {
             "id": 4,
-            "name": "Grab A Table Website",
-            "description": "In this academic project, I worked within a team of 9 to develop a web application that served as a platform for users to find and book with restaurants. During this project, we implemented agile methodologies such as regular meetings, scrums, and taking an iterative approach to the development of the website. We used EJS, html, and css for the front end, Express for the back-end, and MongoDB for the database.",
+            "name": "GrabATable Website",
+            "description": "In this academic project, I worked within a team of 9 to develop a web application that served as a platform for users to find and book with restaurants. \n\nDuring this project, we implemented agile methodologies such as regular meetings, scrums, and taking an iterative approach to the development of the website. We used EJS, html, and css for the front end, Express for the back-end, and MongoDB for the database.",
             "deployed": false,
             "demo": true,
             "siteLink": "",
             "githubLink": "https://github.com/COMP231Team/GrabATable",
             "demoLink": "https://vimeo.com/780006081?share=copy#t=1068",
             "images": ["https://placekitten.com/150/150","https://placekitten.com/150/150","https://placekitten.com/150/150"],
-            "tags": ["CSS", "Demo", "EJS", "Express", "Group Project", "HTML", "JavaScript", "MongoDB", "NoSQL", "Website"]
+            "tags": ["MVC", "CSS", "Demo", "EJS", "Express", "Group Project", "HTML", "JavaScript", "MongoDB", "NoSQL", "Website"]
         },
         {
             "id": 5,
             "name": "Bloodbank Website",
-            "description": "This academic project was created as part of the Java EE course at Centennial College. It allows the user to create, read, update, and delete data relating to a blood bank company. There are three subservices associated with the blood bank: patient service, blood stock service, and blood bank service. SQL was used to create and interact with the database. HTML and CSS was used for the front-end. Spring Framework and Java was used for the backend.",
+            "description": "This academic project was created as part of the Java EE course at Centennial College. It allows the user to create, read, update, and delete data relating to a blood bank company. \n\nThere are three subservices associated with the blood bank: patient service, blood stock service, and blood bank service. SQL was used to create and interact with the database. HTML and CSS was used for the front-end. Spring Framework and Java was used for the backend.",
             "deployed": false,
             "demo": true,
             "siteLink": "",
             "githubLink": "https://github.com/abishekh-manoharan/BloodBankWebsite",
             "demoLink": "https://youtu.be/onljbCRj9jc",
             "images": ["https://placekitten.com/150/150","https://placekitten.com/150/150","https://placekitten.com/150/150"],
-            "tags": ["CSS", "Demo", "HTML", "Java", "SQL", "Individual Project", "Academic Project", "Website"]
+            "tags": ["MVC", "CSS", "Demo", "HTML", "Java", "SQL", "Individual Project", "Academic Project", "Website"]
         },
         {
             "id": 6,
             "name": "Centennial Library Mobile Application",
-            "description": "This academic project was created as part of the Mobile Application Development course at Centennial College. ",
+            "description": "This academic project was created as part of the Mobile Application Development course at Centennial College. It supports 2 types of users: Student and Librarian. The student user type can view and check out books from the library database. The librarian can view and manage the books database. \n\nThe application was programmed using JAVA and has a repository-based MVVM architecture. It uses Room database for persistence and has live-data features.",
             "deployed": false,
             "demo": false,
             "siteLink": "",
             "githubLink": "https://github.com/abishekh-manoharan/BloodBankWebsite",
             "demoLink": "https://youtu.be/onljbCRj9jc",
             "images": ["https://placekitten.com/150/150","https://placekitten.com/150/150","https://placekitten.com/150/150"],
-            "tags": ["Java", "Firebase", "XML", "Mobile Application","Individual Project", "Academic Project"]
+            "tags": ["MVVM", "Java", "Room", "XML", "Mobile Application","Individual Project", "Academic Project"]
         }
     ])
 
