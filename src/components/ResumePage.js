f8c1faf1e8d0aa79b29dea2da7026e4809e72fb5
abi-manoharan97 <abishekh.manoharan@gmail.com>
2023-06-27 11:16:12 -0400

    style adjustments

--- src/components/ResumePage.js
+++ src/components/ResumePage.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import resume from '../files/resume.pdf'
+// import resume from '../files/resume.pdf'
 
 function ResumePage(props) {
     return (
         <div class="resumePage">
-            <embed src={resume} type="application/pdf" width="100%" height="100%" />
+            {/* <embed src={resume} type="application/pdf" width="100%" height="100%" /> */}
         </div>
     );
 }
