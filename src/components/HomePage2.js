24ecafc23c2e70b1390d036b72ebb82b74dd18ea
abi-manoharan97 <abishekh.manoharan@gmail.com>
2023-09-07 09:42:36 -0400

    button sizing bug fix

--- src/components/HomePage2.js
+++ src/components/HomePage2.js
@@ -1,35 +1,41 @@
 import React, { useEffect } from 'react';
 import pic from '../images/profile.png'
 
-function HomePage2({setStylesForLight, setStylesForDark, darkMode}) {
-    useEffect(()=>{
-        if (!darkMode) {
-            setStylesForLight()
-        }
-        else {
-            setStylesForDark()
-        }
-    },[darkMode])
-    return (
-        <div className="homePage">
-          <div className="greeting">Hello! I am</div>
-          <div className="name">
-            <strong>ABISHEKH MANOHARAN</strong>
-          </div>
-          <i className="occupation">Software Developer</i>
-          <div className="about">
-            I am a hard working individual, who is eager to learn an expand my
-            horizons. I find personal satisfaction in creating software that the end
-            user loves, and in working with teams to meet that end.
-          </div>
-          <button className="home-page-btn button">Projects</button>
-          <button className="home-page-btn button2 button">Contact Me</button>
-          <div className="profile">
-            <img src={pic} id="profile-pic" />
-            <div id='profile-pic-outline'/>
-          </div>
-        </div>
-      );
+function HomePage2({ setStylesForLight, setStylesForDark, darkMode, setContent }) {
+
+  useEffect(() => {
+    if (!darkMode) {
+      setStylesForLight()
+    }
+    else {
+      setStylesForDark()
+    }
+  }, [darkMode])
+
+  const handleProjectsBtnClick = () => {
+    setContent('projects')
+  }
+
+  return (
+    <div className="homePage">
+      <div className="greeting">Hello! I am</div>
+      <div className="name">
+        <strong>ABISHEKH MANOHARAN</strong>
+      </div>
+      <i className="occupation">Software Developer</i>
+      <div className="about">
+        I am a hard working individual, who is eager to learn an expand my
+        horizons. I find personal satisfaction in creating software that the end
+        user loves, and in working with teams to meet that end.
+      </div>
+      <button className="home-page-btn button" onClick={handleProjectsBtnClick}>Projects</button>
+      {/* <button className="home-page-btn button2 button">Contact Me</button> */}
+      <div className="profile">
+        <img src={pic} id="profile-pic" />
+        <div id='profile-pic-outline' />
+      </div>
+    </div>
+  );
 }
 
 export default HomePage2;
