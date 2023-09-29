26336781962c8654b3d815dad00c957373f6da28
abi-manoharan97 <abishekh.manoharan@gmail.com>
2023-09-29 15:23:19 -0400

    basic nav bar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,6 +5,7 @@ import ProjectsPage from './ProjectsPage'
 import ContactPage from './ContactPage';
 import ResumePage from './ResumePage';
 import Footer from './Footer';
+import Nav from './Nav';
 import lightbg from '../images/bg-light.jpeg'
 import darkbg from '../images/bg-dark.jpeg'
 import lightLogo from '../images/darkmode-light.svg'
@@ -13,20 +14,29 @@ import darkLogo from '../images/darkmode-dark.svg'
 function Layout(props) {
     const [content, setContent] = useState('home')
     const [darkMode, setDarkMode] = useState(false)
+    const [navOpen, setNavOpen] = useState(false);
+
     let contentToShow = <HomePage2 />
 
-    useEffect(()=>{
+    useEffect(() => {
         if (!darkMode) {
             setStylesForLight()
         }
         else {
             setStylesForDark()
         }
-    },[darkMode])
-    
+    }, [darkMode])
+
     const navBarItemClickHandler = (e) => {
         setContent(e)
     }
+    //handler to open/close mobile navbar
+    const navClick = () => {
+        setNavOpen(!navOpen);
+        navOpen
+            ? (document.getElementById("nav-bar").style.width = "100%")
+            : (document.getElementById("nav-bar").style.width = "0px");
+    };
 
     const setStylesForDark = () => {
         document.getElementById('App').style = "color: white;"
@@ -36,9 +46,9 @@ function Layout(props) {
         document.querySelectorAll('.header').forEach(element => {
             element.style = "color: white;"
         });
-        document.getElementById('background-overlay').style="background-color: #0000007d;" 
+        document.getElementById('background-overlay').style = "background-color: #0000007d;"
     }
-    
+
     const setStylesForLight = () => {
         document.getElementById('App').style = "color: black;"
         document.querySelectorAll('.button').forEach(element => {
@@ -47,28 +57,29 @@ function Layout(props) {
         document.querySelectorAll('.header').forEach(element => {
             element.style = "color: black;"
         });
-        document.getElementById('background-overlay').style="background-color: #rgba(255, 255, 255, 0.150);" 
+        document.getElementById('background-overlay').style = "background-color: #rgba(255, 255, 255, 0.150);"
     }
 
 
     const darkModeHandler = () => {
         setDarkMode(!darkMode)
     }
-    
+
     if (content === 'home') { contentToShow = <HomePage2 setStylesForDark={setStylesForDark} setStylesForLight={setStylesForLight} darkMode={darkMode} setContent={setContent} /> }
-    else { contentToShow = <ProjectsPage setStylesForDark={setStylesForDark} setStylesForLight={setStylesForLight} darkMode={darkMode}/> }
+    else { contentToShow = <ProjectsPage setStylesForDark={setStylesForDark} setStylesForLight={setStylesForLight} darkMode={darkMode} /> }
 
     return (
         <div class="layout">
+            {/* // desktop navbar */}
             <div class="navbar">
                 {/* <button class="navbarItem" onClick={()=>navBarItemClickHandler('home')}>Logo</button> */}
                 <button class="navbarItem button" onClick={() => navBarItemClickHandler('home')}>Home</button>
                 <button class="navbarItem button" onClick={() => navBarItemClickHandler('projects')}>Projects</button>
-                <a id="navbarItem-github-btn"href="https://github.com/abishekh-manoharan/portfolio-website" target='_'><button class="navbarItem button" >GitHub</button></a>
+                <a id="navbarItem-github-btn" href="https://github.com/abishekh-manoharan/portfolio-website" target='_'><button class="navbarItem button" >GitHub</button></a>
                 <button class="navbarItem button" id="darkModeLogo" onClick={darkModeHandler}>
-                    {darkMode ? 
-                    <img src={lightLogo} className="darkModeLogo"/>  
-                    : <img src={darkLogo} className="darkModeLogo"/>  
+                    {darkMode ?
+                        <img src={lightLogo} className="darkModeLogo" />
+                        : <img src={darkLogo} className="darkModeLogo" />
                     }
                 </button>
                 {/* <img src={lightLogo} class="navbarItem button" onClick={darkModeHandler}/> */}
@@ -76,6 +87,10 @@ function Layout(props) {
                 {/* <button class="navbarItem" onClick={()=>navBarItemClickHandler('contacts')}>Contact</button> */}
             </div>
 
+            {/* // mobile navbar */}
+            <button class="nav-btn" onClick={navClick}>
+                Nav
+            </button>
             <div class="content">
                 {contentToShow}
             </div>
@@ -87,14 +102,15 @@ function Layout(props) {
             {darkMode ?
                 <div className="bg">
                     <div id="background-overlay" />
-                    <img src={darkbg} className="background-img"/>
+                    <img src={darkbg} className="background-img" />
                 </div>
-                : 
+                :
                 <div className="bg">
                     <div id="background-overlay" />
-                    <img src={lightbg} className="background-img"/>
+                    <img src={lightbg} className="background-img" />
                 </div>
             }
+            <Nav />
         </div>
     );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Nav(props) {
+    return (
+        <div id="nav-bar">
+          <button class="nav-option">Home</button>
+          <button class="nav-option">Projects</button>
+          <button class="nav-option">GitHub</button>
+        </div>
+      );
+}
+
+export default Nav;
