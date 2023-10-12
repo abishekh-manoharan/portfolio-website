d4ca25a0fdd9e0db4f531ed08e648e1b9235f7ef
abi-manoharan97 <abishekh.manoharan@gmail.com>
2023-10-12 11:22:47 -0400

    home, github mobile menu options functionality

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,12 +5,16 @@ function Nav({ darkMode, darkModeHandler, setContent, setNavOpen }) {
     setContent('projects')
     setNavOpen(false)
   }
+  const handleHomeBtnClick = () => {
+    setContent('home')
+    setNavOpen(false)
+  }
 
   return (
     <div id="nav-bar">
-      <button class="nav-option">Home</button>
+      <button class="nav-option" onClick={handleHomeBtnClick}>Home</button>
       <button class="nav-option" onClick={handleProjectsBtnClick}>Projects</button>
-      <button class="nav-option">GitHub</button>
+      <a class="nav-option-a" href="https://github.com/abishekh-manoharan/portfolio-website" target="_"><button class="nav-option" style={{margin:'auto'}}>GitHub</button></a>
       <button class="nav-option" onClick={darkModeHandler}>Dark Mode</button>
     </div>
   );
